38086e9282a3d75c4234c2b5bf4e6e03a94a2e97
Leggera <lana.krlv1995@gmail.com>
2016-11-18 18:02:58 +0300

    Add files via upload

--- graph.pl
+++ graph.pl
@@ -1,6 +1,6 @@
 edge1(a, b, 1).
-edge1(a, c, 4).
-edge1(b, c, 3).
+edge1(a, c, 3).
+edge1(c, d, 3).
 
 edge(X, Y, N):-edge1(X, Y, N);edge1(Y, X, N).
 
@@ -14,3 +14,38 @@ path(X, Y, N, P):-path5(X, Y, [], N, P).
 path5(X, X, [], 0, []).
 path5(X, Y, _, N, [X, Y]):-edge(X, Y, N).
 path5(X, Y, L, N, [X|P]):-edge(X, X1, N1), not(member(X1, L)), dif(Y, X1), path5(X1, Y,[X|L], N2, P), not(member(X, P)), N is N1+N2.
+
+short_path(X, Y, [X, Y]):-edge(X, Y, _), !.
+
+short_path([X|V], Y, LV):-findall(X1, edge(X, X1, _), P), member(Y, P); short_path(V, Y, LV), !.
+short_path([X|_], Y, LV):-findall(X1, edge(X, X1, _), P), short_path(P, Y, LV).
+/*short_path(X, Y, [X1|L]):-findall(X1, edge(X, X1, _), V), short_path(V, Y, L).*/
+short_path(X, Y, _):-findall(X1, edge(X, X1, _), V), (not(member(Y, V))),!/*?*/ , short_path(V, Y, _).
+/*short_path([X|V], Y, P):-(findall(X1, edge(X, X1, _), V1), short_path(V1, Y, P1);short_path(V, Y, P2)), append(P1, P2, P).*/
+
+edges2([], []):-!.
+edges2([X|L], V):-edges2(X, V1), edges2(L, V2), V=[[X|V1]|V2], !.
+edges2(X, [X|V]):-findall(X1, edge(X, X1, _), V),!.
+sh_path([] ,_, []):-!.
+sh_path(X, Y, [X, Y]):-edge(X, Y, _), !.
+sh_path(X, Y, P):-edges2(X, V),((members(Y, V, Ex), P = Ex, print(Ex),!) ;(sh_path(V, Y, L), P=L, print(L))).
+
+members(_, [],[]).
+members(Y, [[X|V1]|V], [X|Ex]):-member(Y, V1),!, members(Y, V, Ex).
+members(Y, [[_|_]|V], Ex):-members(Y, V, Ex), !.
+members(Y, [X|V1], [X]):-member(Y, V1).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
